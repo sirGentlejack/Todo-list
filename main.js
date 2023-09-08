bc28e9062baefaff0db6ead03a7ad4020679cea0
sirGentlejack <fagbemiilemobayo@gmail.com>
2023-09-08 19:35:39 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log('My Todo List!');
+
+function get_todos() {
+  var todos = new Array;
+  var todos_str = localStorage.getItem('todo');
+  if (todos_str !== null){
+    todos = JSON.parse(todos_str);
+  }
+  return todos;
+}
+
+function add() {
+  var task = document.getElementById('task').value;
+  
+  var todos = get_todos();
+  todos.push(task);
+  localStorage.setItem('todo', JSON.stringify(todos));
+  
+  show();
+  
+  return false;
+}
